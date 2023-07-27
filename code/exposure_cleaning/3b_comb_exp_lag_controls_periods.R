f7b164ce62dc3118e22cbe28ca2ed970d76982e3
sofiavega98 <54519185+sofiavega98@users.noreply.github.com>
2023-07-27 11:47:26 -0400

    Editing 3b

--- code/exposure_cleaning/3b_comb_exp_lag_controls_periods.R
+++ code/exposure_cleaning/3b_comb_exp_lag_controls_periods.R
@@ -56,6 +56,9 @@ for(i in 1:nrow(county_wf_summarized_date_array)){
   end_date <- county_wf_summarized_date_array$end_date[i]
   
   # Control 1
+  control1_start <- control_dates_df$control1_start[i]
+  control1_end <- control_dates_df$control1_end[i]
+  
   if(is.na(as.Date(control_dates_df$control1_start[i]))){
     date <- rep(NA,lag_num)
   }else{
@@ -65,31 +68,42 @@ for(i in 1:nrow(county_wf_summarized_date_array)){
   #t <- seq(1,lag_num)
   
   # Define control period for exposure period
-  control1_period <- data.frame(cbind(GEOID, start_date = as.character(start_date), end_date = as.character(end_date), date = as.character(date[1]), t(lags[1,]) ))
+  control1_period <- data.frame(cbind(GEOID, start_date = as.character(control1_start), end_date = as.character(control1_end), 
+                                      date = as.character(date[1]), t(lags[1,]) ))
   
   # Define control days for lag days
-  control1 <- data.frame(cbind(GEOID, start_date = as.character(start_date), end_date = as.character(end_date), date = as.character(date[(length(date)-6):length(date)]), lags[2:8,] ))
+  control1 <- data.frame(cbind(GEOID, start_date = as.character(control1_start), end_date = as.character(control1_end),
+                               date = as.character(date[(length(date)-6):length(date)]), lags[2:8,] ))
   
   # Combine to have final control1
   control1 <- rbind(control1_period,control1)
   
   # Control 2
+  control2_start <- control_dates_df$control2_start[i]
+  control2_end <- control_dates_df$control2_end[i]
+  
   if(is.na(as.Date(control_dates_df$control2_start[i]))){
     date <- rep(NA,lag_num)
   }else{
     date <- seq(as.Date(control_dates_df$control2_start[i]), as.Date(control_dates_df$control2_end[i]), by="days")
   }
   # Define control period for exposure period
-  control2_period <- data.frame(cbind(GEOID, start_date = as.character(start_date), end_date = as.character(end_date), date = as.character(date[1]), t(lags[1,]) ))
+  control2_period <- data.frame(cbind(GEOID, start_date = as.character(control2_start), end_date = as.character(control2_end), 
+                                      date = as.character(date[1]), t(lags[1,]) ))
   
   # Define control days for lag days
-  control2 <- data.frame(cbind(GEOID, start_date = as.character(start_date), end_date = as.character(end_date), date = as.character(date[(length(date)-6):length(date)]), lags[2:8,] ))
+  control2 <- data.frame(cbind(GEOID, start_date = as.character(control2_start), end_date = as.character(control2_end), 
+                               date = as.character(date[(length(date)-6):length(date)]), lags[2:8,] ))
   
   # Combine to have final control1
   control2 <- rbind(control2_period,control2)
   
   # Combine control 1 and 2
   temp <- rbind(control1, control2)
+  
+  # Define wildifire id
+  temp$wfGEOID_id <- i
+  
   WF_control <- rbind(WF_control, temp)
   
 }
@@ -106,12 +120,42 @@ WF_control$event_lag4 <- as.double(WF_control$event_lag4)
 WF_control$event_lag5 <- as.double(WF_control$event_lag5)
 WF_control$event_lag6 <- as.double(WF_control$event_lag6)
 WF_control$event_lag7 <- as.double(WF_control$event_lag7)
-## stop here
+
+
+# Add wfGEOID_id to exposure data
+# Initialize 
+WF_exposed_full$wfGEOID_id <- NA
+for(i in 1:nrow(county_wf_summarized_date_array)){
+  
+  # Filter by GEOID and start_date
+  # county_wf_summarized_date_array has the info for each exposure period so we can 
+  # cycle through each row to find each wf
+  GEOID_temp <- county_wf_summarized_date_array$GEOID[i]
+  start_date_temp <- county_wf_summarized_date_array$start_date[i]
+  # Assign wfGEOID_id
+  #exp_model_input %>% filter(GEOID == GEOID_temp, start_date == start_date_temp) %>% mutate(wfGEOID_id = i)
+  WF_exposed_full$wfGEOID_id[WF_exposed_full$GEOID == GEOID_temp & WF_exposed_full$start_date == start_date_temp] <- i
+  
+}
+
+
 # Combine with exposure data
-
-
 exp_model_input <- rbind(WF_exposed_full,WF_control)
 
+# Add indicators for control
+exp_model_input$control_indicator <- 0
+exp_model_input <- exp_model_input %>% 
+  mutate( control_indicator = ifelse(event_lag0 == 0 & event_lag1 == 0 & event_lag2 == 0 & event_lag3 == 0 & 
+            event_lag4 == 0 & event_lag5 == 0 & event_lag6 == 0 & event_lag7 == 0, 1,0) )
+
+
+# Remove rows with date = NA
+# This are rows where a control was not found
+exp_model_input <- exp_model_input %>% drop_na(date,)# Remove rows with date = NA
+
+
+
 save(exp_model_input, file = paste0(dir_out,"exp_model_input_periods.RData"))
+
 
 
